b811c4d823b7482314b97fe32d1d2c32e9fc60c7
marcelo.vergara <marcelorv@gmail.com>
2026-06-24 13:32:32 -0300

    feat: enhance project card interactivity and styling

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, HostListener } from '@angular/core';
 import { Project } from '../../data/projects.data';
 
 @Component({
@@ -9,6 +9,18 @@ import { Project } from '../../data/projects.data';
 })
 export class ProjectCard {
   project = input.required<Project>();
+  overlayOpen = signal(false);
+
+  toggle() {
+    this.overlayOpen.update(v => !v);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(e: MouseEvent) {
+    if (!(e.target as Element).closest('app-project-card')) {
+      this.overlayOpen.set(false);
+    }
+  }
 
   protected getNodeById(id: string) {
     return this.project().nodes.find(n => n.id === id);
